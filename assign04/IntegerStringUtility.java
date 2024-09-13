f7adc94dc44d6944e58284986156279ffeb3bc22
isaacdbeans <isaacdbeans@gmail.com>
2024-09-13 12:34:59 -0600

    first comparator

--- assign04/IntegerStringUtility.java
+++ assign04/IntegerStringUtility.java
@@ -17,7 +17,9 @@ public class IntegerStringUtility {
         }
     }
     public static <T> T findMax(T[] arr, Comparator<? super T> cmp) throws NoSuchElementException {
-        
+        T[] clone = arr.clone();
+        insertionSort(clone, cmp);
+        return clone[clone.length-1];
     }
     public static String[][] getSimilarityGroups(String[] arr){
 
@@ -30,6 +32,49 @@ public class IntegerStringUtility {
 
         @Override
         public int compare(String s, String t1) {
+            char[] arr1 = s.toCharArray();
+            char[] arr2 = t1.toCharArray();
+            char[]arr1NoZero = new char[0];
+            char[]arr2NoZero = new char[0];
+
+            for (int i = 0; i < arr1.length; i++) {
+                if(arr1[i] == 0) {
+                    continue;
+                }
+                arr1NoZero = new char[arr1.length - i];
+                for (int j = i; j < arr1.length; j++) {
+                    arr1NoZero[j-i] = arr1[j];
+                }
+                break;
+            }
+
+            for (int i = 0; i < arr2.length; i++) {
+                if(arr2[i] == 0) {
+                    continue;
+                }
+                arr2NoZero = new char[arr2.length - i];
+                for (int j = i; j < arr2.length; j++) {
+                    arr2NoZero[j-i] = arr2[j];
+                }
+                break;
+            }
+
+            if(arr1NoZero.length > arr2NoZero.length) {
+                return 1;
+            }
+
+            if(arr1NoZero.length < arr2NoZero.length) {
+                return -1;
+            }
+
+            for (int i = 0; i < arr1NoZero.length; i++) {
+                if (arr1NoZero[i] > arr2NoZero[i]) {
+                    return 1;
+                }
+                if (arr1NoZero[i] < arr2NoZero[i]) {
+                    return -1;
+                }
+            }
             return 0;
         }
     }
