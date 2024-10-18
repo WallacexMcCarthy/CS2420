f91b70e09fa2c72022e5556b9d3cce2a53ff99a5
isaacdbeans <isaacdbeans@gmail.com>
2024-10-18 15:51:19 -0600

    Setup assign07

---
+++ assign07/GraphUtility.java
@@ -0,0 +1,17 @@
+package assign07;
+
+import java.util.List;
+
+public class GraphUtility {
+    public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
+
+    }
+
+    public static <Type> List<Type> shortestWeightedPath(List<Type> sources, List<Type> destinations, List<Double> weights, Type srcData, Type dstData) {
+
+    }
+
+    public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) {
+
+    }
+}
