fe5ae1db9823b0c1f28d0e49a0099137fd73f1b4
Wallace McCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2024-11-25 10:58:55 -0700

    sutf

--- assign10/SimpleMethodsTiming.java
+++ assign10/SimpleMethodsTiming.java
@@ -9,7 +9,7 @@ public class SimpleMethodsTiming extends TimingExperiment{
     private static int problemSizeMin = 1000;
     private static int problemSizeCount = 20;
     private static int problemSizeStep = 1000;
-    private static int experimentIterationCount = 20;
+    private static int experimentIterationCount = 100;
     protected BinaryMinHeap<Integer> heap;
     protected ArrayList<Integer> array;
     private Random rng;
@@ -28,14 +28,14 @@ public class SimpleMethodsTiming extends TimingExperiment{
         heap = new BinaryMinHeap<>();
         array = new ArrayList<>();
         rng = new Random();
-        for (int i = problemSize; i > 0; i--) { // worst case add
-            array.add(i);
-        }
-//        for (int i = 0; i < problemSize; i++) { // all others
-//            array.add(rng.nextInt());
+//        for (int i = problemSize; i > 0; i--) { // worst case add
+//            array.add(i);
 //        }
+        for (int i = 0; i < problemSize; i++) { // all others
+            array.add(rng.nextInt());
+        }
 
-//        heap = new BinaryMinHeap<>(array); // extract and peek
+        heap = new BinaryMinHeap<>(array); // extract and peek
 
     }
 
@@ -44,18 +44,18 @@ public class SimpleMethodsTiming extends TimingExperiment{
      */
     @Override
     protected void runComputation() {
-        for (int i = 0; i < array.size(); i++) { // both adds
-            heap.add(array.get(i));
-        }
+//        for (int i = 0; i < array.size(); i++) { // both adds
+//            heap.add(array.get(i));
+//        }
 //        for (int i = 0; i < array.size(); i++) { // extract
 //            heap.extract();
 //        }
-//        for (int i = 0; i < array.size(); i++) { // peek compiler kept optimizing only peek calls so extracting a little stops it
-//            if (i % 1000 == 0) {
-//                heap.extract();
-//            }
-//            heap.peek();
-//        }
+        for (int i = 0; i < array.size(); i++) { // peek compiler kept optimizing only peek calls so extracting a little stops it
+            if (i % 1000 == 0) {
+                heap.extract();
+            }
+            heap.peek();
+        }
 
     }
 
